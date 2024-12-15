113b8a699f4f5b701f9e9007c07aecf378048c94
Ige-Joseph <119541924+Ige-Joseph@users.noreply.github.com>
2024-12-15 18:35:16 +0100

    Update settings.py and Database config.
    Updated Database

--- crud/settings.py
+++ crud/settings.py
@@ -89,12 +89,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres.fcijaahovvuwyejvtzyt',
-        'PASSWORD': 'ZxeDpFzbeOGAllsa',
-        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
+        'USER': 'postgres.jrxxvfwxwgjijzcdjcrt',
+        'PASSWORD': 'Victoryandgrace567',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
         'PORT': '6543'
     }
 }
+
 
 
 # Password validation
